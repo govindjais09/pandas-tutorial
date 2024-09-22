6aa68686d545bd2eed10bccaa51844cdfdd08fb3
govindjais09 <dashingjais@gmail.com>
2024-09-22 14:58:47 +0530

    PandasDataFrameAttrMethods completed

---
+++ PandasDataFrameAttrMethods/Demo1.py
@@ -0,0 +1,16 @@
+#pandas dataframe dtypes attribute
+
+import pandas as pd
+
+#dataset
+data = {
+    'student' : ["Amit" , "John" , "Jacob" , "David" , "Steve"] ,
+    'rank' : [1,2,3,4,5],
+    'marks' : [95,85,75,60,55]
+}
+
+df=pd.DataFrame(data , index=['rowA','rowB','rowC','rowD','rowE'])
+
+print("Students records\n\n",df)
+
+print("DataTypes", df.dtypes)
---
+++ PandasDataFrameAttrMethods/Demo5.py
@@ -0,0 +1,17 @@
+#pandas dataframe index attribute
+from operator import index
+
+import pandas as pd
+
+#dataset
+data = {
+    'student' : ["Amit" , "John" , "Jacob" , "David" , "Steve"] ,
+    'rank' : [1,2,3,4,5],
+    'marks' : [95,85,75,60,55]
+}
+
+df=pd.DataFrame(data , index=['rowA','rowB','rowC','rowD','rowE'])
+
+print("Students records\n\n",df)
+
+print("Index Names" , df.index)
