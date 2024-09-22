6c129b5920e73056ed59d0c6b139ba2eb5962242
govindjais09 <dashingjais@gmail.com>
2024-09-22 13:28:12 +0530

    chapter 1 completed pandasdatafraame

---
+++ PandasDataFrame/Demo2.py
@@ -0,0 +1,16 @@
+#create a pandas dataframe
+
+import pandas as pd
+
+data = {
+    'student' : ["Amit" , "John" , "Jacob" , "David" , "Steve"] ,
+    'rank' : [1,2,3,4,5],
+    'marks' : [95,85,75,60,55]
+
+}
+
+df=pd.DataFrame(data , index = ["rowA" , "rowB" , "rowC" , "rowD" , "rowE"])
+
+print("Student Records\n\n" ,  df)
+
+print("\nValue = " , df.loc["rowA"])
---
+++ PandasDataFrame/Demo5.py
@@ -0,0 +1,17 @@
+#iterate a panda dataframe to display column name
+
+import pandas as pd
+
+data = {
+    'student' : ["Amit" , "John" , "Jacob" , "David" , "Steve"] ,
+    'rank' : [1,2,3,4,5],
+    'marks' : [95,85,75,60,55]
+
+}
+
+df=pd.DataFrame(data , index = ["s1" , "s2" , "s3" , "s4" , "s5"])
+
+print("\nStudent Records by iterating column names\n\n")
+
+for col in df:
+    print(col)
